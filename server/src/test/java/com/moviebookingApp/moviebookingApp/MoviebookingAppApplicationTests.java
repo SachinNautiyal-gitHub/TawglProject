a7f274ad1432d8aa79ce85fd42685f2440ec4821
Sachin Nautiyal <nautiyalsachin217@gmail.com>
2024-10-25 08:24:27 +0530

    changes

---
+++ server/src/test/java/com/moviebookingApp/moviebookingApp/MoviebookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebookingApp.moviebookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
