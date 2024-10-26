feb8f76d7110b4c5b03299076e29db2f863d5764
Sachin Nautiyal <nautiyalsachin217@gmail.com>
2024-10-26 17:15:41 +0530

    changes

---
+++ server/moviesticketbooking/src/main/java/com/sachinnautiyal/moviesticketbooking/MoviesticketbookingApplication.java
@@ -0,0 +1,13 @@
+package com.sachinnautiyal.moviesticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesticketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesticketbookingApplication.class, args);
+	}
+
+}
---
+++ server/moviesticketbooking/src/test/java/com/sachinnautiyal/moviesticketbooking/MoviesticketbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinnautiyal.moviesticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesticketbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
